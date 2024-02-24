1815ed308ddfc0f69fbdac7abe08be6838b71ea7
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2024-02-24 14:10:48 +0530

    test

--- docker_script.sh
+++ docker_script.sh
@@ -1,36 +1,32 @@
-#!/bin/bash
+# #!/bin/bash
 
 IMAGE=$IMAGE_NAME
 
 echo "****************************************************************"
-echo "Removing the existing docker container for the popcorn-data-server"
+echo "***** Building the docker image for the popcorn-data-server ****"
 echo "****************************************************************"
-docker rm -f $IMAGE
+docker build -t $IMAGE:$BUILD_ID .
 
 echo "****************************************************************"
-echo "Removing the existing docker image for the popcorn-data-server"
+echo "**************** PUSHING IMAGE TO DOCKERHUB ********************"
 echo "****************************************************************"
-docker rmi -f $IMAGE
-
-echo "****************************************************************"
-echo "Building the docker image for the popcorn-data-server"
-echo "****************************************************************"
-docker build -t $IMAGE .
-
-echo "****************************************************************"
-echo "PUSHING IMAGE TO DOCKERHUB"
-echo "****************************************************************"
-docker tag $IMAGE:latest pratikgaikwad/$IMAGE:latest
+docker tag $IMAGE:$BUILD_ID pratikgaikwad/$IMAGE:$BUILD_ID
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push pratikgaikwad/$IMAGE
+docker push pratikgaikwad/$IMAGE:$BUILD_ID
 
 echo "****************************************************************"
-echo "Restarting docker compose "
+echo "**************** Stoping previous containers *******************"
 echo "****************************************************************"
-cd /var/jenkins_home/popcorn
+docker stop popocorn-api
 
-docker compose down
-docker rmi $IMAGE
-docker compose pull
-docker compose up --force-recreate --build -d
-docker image prune -f
+sleep 5
+
+echo "****************************************************************"
+echo "*********************** Deploying Image ************************"
+echo "****************************************************************"
+docker run -d -p 7000:7000 --rm -v popcorn_popcorn_images:/app/images -v popcorn_popcorn_videos:/app/videos --network popcorn_network --name popocorn-api pratikgaikwad/$IMAGE:$BUILD_ID
+
+echo "****************************************************************"
+echo "**************** Removing previous image *******************"
+echo "****************************************************************"
+docker image prune -a -f
